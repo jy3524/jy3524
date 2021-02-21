11ab83e2d0f2de49c6785346870d8304e97bba59
jy3524 <70925376+jy3524@users.noreply.github.com>
2021-02-21 11:57:02 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var canvas;
+
+function setup() {
+	canvas = createCanvas(windowWidth, windowHeight);
+	canvas.position(0,0);
+	canvas.style('z-index', '-1');
+	background(143,188,143);
+}
+
+function draw () {
+	noStroke();
+	fill(255,255,255,30);
+	ellipse(mouseX,mouseY,30,30);
+}
