cc956417406715773b7e2a23ee29488e107d34dc
jy3524 <70925376+jy3524@users.noreply.github.com>
2021-05-01 19:02:56 -0400

    Create sketch.js

---
+++ Final/sketch.js
@@ -0,0 +1,21 @@
+$(".nav .nav-link").on("click", function() {
+    $(".nav").find(".active").removeClass("active");
+    $(this).addClass("active");
+});
+
+const toggleBtn = document.querySelector('.navbar__toggleBtn');
+const menu = document.querySelector('.nav')
+const chevron = document.querySelector('.fa-chevron-down');
+const linkedin = document.querySelector('.fa-linkedin');
+
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
+chevron.addEventListener('click', () => {
+    location.href='#portfolio';
+});
+
+linkedin.addEventListener('click', () => {
+    location.href="https://www.linkedin.com/in/masahitoyoon";
+});
